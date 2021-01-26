8bc3e98c4ab66000e3cf67dde90b3084d6700adf
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2021-01-26 12:15:41 -0700

    Initial solution, need to refactor

---
+++ summy.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+def summy(string_of_ints)
+  new_array = []
+  a = string_of_ints.split.map do |element|
+    element.to_i
+  end
+  a.sum
+end
+
+summy("1 2 3")
+summy("1 2 3 4")
+summy("1 2 3 4 5")
+summy("10 10")
+summy("0 0")
+
+
+# Write a function that takes a string which has integers inside it separated by spaces,
+# and your task is to convert each integer in the string into an integer and return their sum.
+#
+# Example
+#
+# summy("1 2 3")  ==> 6
