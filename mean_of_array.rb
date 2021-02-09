04a68dfaf717ef7aa0b188160390f36d44b24cdf
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2021-02-09 11:49:20 -0700

    Final

---
+++ mean_of_array.rb
@@ -0,0 +1,8 @@
+require 'pry'
+
+def get_average(marks)
+  marks.sum / marks.size
+  # raise NotImplementedError.new "TODO: get_average"
+end
+
+get_average([2, 2, 2, 2])
--- wave.rb
+++ wave.rb
@@ -4,14 +4,30 @@ def wave(str)
   result = []
   counter = -1
   chars = str.downcase.split('')
+  test = str.gsub(/\s+/, '')
+  new_string = []
   chars.length.times do
     counter += 1
-    new_string = []
-    chars.each do |char|
-      binding.pry
+    if chars[counter] == " "
+      counter += 1
     end
+    if chars[counter] != nil
+      chars[counter] = chars[counter].upcase
+    end
+    if counter > 0 && chars[counter - 1] != " "
+      
+      chars[counter - 1] = chars[counter - 1].downcase
+    else
+      chars[counter - 2] = chars[counter - 2].downcase
+    end
+    new_string << chars.join
   end
+  if test.length != new_string.count
+    new_string.pop
+  end
+    new_string
 end
 
-
-wave("hello")
+# p wave("hello")
+# p wave("codewars")
+p wave("two words")
